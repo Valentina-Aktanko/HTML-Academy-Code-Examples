e42d9d4421e1b9ab8a0c568f5158181fa1c806bd
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-11-22 21:16:18 +0300

    adds id of product

--- gloevk/product.php
+++ gloevk/product.php
@@ -2,8 +2,9 @@
 require('products_db.php');
 require('components/header.php');
 
-$title = get_product_title(1);
-$img_url = get_img_url(10);
+$id = 1;
+$title = get_product_title($id);
+$img_url = get_img_url($id);
 
 require('components/product_info.php');
 require('components/footer.php');
