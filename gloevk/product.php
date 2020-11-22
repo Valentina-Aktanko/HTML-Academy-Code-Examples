915e2f79e278c91badb0df4fd99a7c581d25ad87
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-11-22 21:13:17 +0300

    displays product image and title with php variable

--- gloevk/product.php
+++ gloevk/product.php
@@ -3,6 +3,7 @@ require('products_db.php');
 require('components/header.php');
 
 $title = get_product_title(1);
+$img_url = get_img_url(10);
 
 require('components/product_info.php');
 require('components/footer.php');
