d5514955276a889ec4efaa3860d9a7aca9fd0fbf
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-11-22 11:50:02 +0300

    returns the old content to the main page

--- gloevk/index.php
+++ gloevk/index.php
@@ -1,6 +1,5 @@
 <?php
 require('components/header.php');
-// require('components/company_info.php');
-require('components/company_info_detail.php');
+require('components/company_info.php');
 require('components/footer.php');
 ?>
