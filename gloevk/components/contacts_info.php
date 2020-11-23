1321e954dd6ab408243a799cdbbcf2808dd223d6
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2020-11-23 11:36:39 +0300

    fixes image paths and link source

--- gloevk/components/contacts_info.php
+++ gloevk/components/contacts_info.php
@@ -5,7 +5,7 @@
       <p>Нас сложно найти, легко потерять и невозможно забыть.</p>
       <cite class="author">— Джонсон Схетхем, CEO gloevk</cite>
     </blockquote>
-    <img src="./контакты_files/map.jpg" width="510" height="240" alt="Санкт-Петербург, улица Большая Конюшенная, 19/8">
+    <img src="img/map.jpg" width="510" height="240" alt="Санкт-Петербург, улица Большая Конюшенная, 19/8">
     <a class="map-link" href="http://www.2gis.ru/" target="_blank" title="2ГИС — Городской информационный сервис">© 2ГИС — Городской информационный сервис</a>
     <div class="contacts-info">
       <p class="contact-phone">+8 (900) 000-00-00</p>
--- gloevk/components/footer.php
+++ gloevk/components/footer.php
@@ -3,8 +3,8 @@
         <p class="copyright">© Кекс, 2019</p> 
         <ul class="navigation-list">
           <li><a href="catalog.php">Каталог</a></li>
-          <li><a href="delivery.html">Доставка</a></li>
-          <li><a href="contacts.html">Контакты</a></li>
+          <li><a href="delivery.php">Доставка</a></li>
+          <li><a href="contacts.php">Контакты</a></li>
         </ul>
         <ul class="social-list">
           <li>
