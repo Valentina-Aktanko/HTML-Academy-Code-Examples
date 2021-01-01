803df9383d22bbbcba02f16b7e87e334be03fa31
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2021-01-01 22:11:07 +0300

    refactor: tooltip script, event click delegation

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,15 +1,18 @@
 let tooltip = document.querySelector('.tooltip');
 let closeButton = document.querySelector('.close-button');
-let tooltipButtons = document.querySelectorAll('.tooltip-button');
+let tooltipContainer = document.querySelector('.tooltip-container');
 let tooltipText = document.querySelector('.tooltip-text');
 
 closeButton.onclick = function () {
   tooltip.classList.remove('opened');
 };
 
-for (let tooltipButton of tooltipButtons) {
-  tooltipButton.onclick = function () {
-    tooltipText.textContent = tooltipButton.dataset.tooltipText;
-    tooltip.classList.add('opened');
-  }
+if (tooltipContainer) {
+  tooltipContainer.onclick = function (e) {
+    let tooltipButton = e.target.closest('.tooltip-button');
+    if (tooltipButton) {
+      tooltipText.textContent = tooltipButton.dataset.tooltipText;
+      tooltip.classList.add('opened');
+    }
+  };
 };
