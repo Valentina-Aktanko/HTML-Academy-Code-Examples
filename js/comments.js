e25bedd4605191c6349eefb2ee96a912610186e1
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2021-01-01 22:55:58 +0300

    feat: counts the number of characters in a comment

--- js/comments.js
+++ js/comments.js
@@ -1,6 +1,11 @@
 let commentForm = document.querySelector('.comment-form');
 let commentList = document.querySelector('.comment-list');
 let commentField = document.querySelector('.comment-field');
+let charCounter = document.querySelector('.char-counter');
+
+function setCharCount() {
+  charCounter.textContent = commentField.value.length;
+}
 
 commentForm.onsubmit = function (evt) {
   evt.preventDefault();
@@ -8,8 +13,16 @@ commentForm.onsubmit = function (evt) {
   let newComment = document.createElement('li');
   newComment.classList.add('user-comment');
   newComment.textContent = commentField.value;
-  commentField.value= '';
-
+  commentField.value = '';
   commentList.append(newComment);
 };
 
+window.onload = function() {
+  if (commentField.value.length) {
+    setCharCount()
+  }
+}
+
+commentField.oninput = function () {
+  setCharCount()
+};
