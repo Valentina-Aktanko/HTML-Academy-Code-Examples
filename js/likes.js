07fcb8d45676140cefe194508ed486b177e7b203
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2021-01-01 17:36:59 +0300

    aria-pressed attribute for like button

--- js/likes.js
+++ js/likes.js
@@ -1,11 +1,16 @@
 let heart = document.querySelector('.heart');
 let likesNumber = document.querySelector('.likes-number');
 
-heart.onclick = function () {
+heart.onclick = function (e) {
+  // Изменение лайков
   if (heart.classList.contains('added')) {
     likesNumber.textContent--;
   } else {
     likesNumber.textContent++;
   }
   heart.classList.toggle('added');
+
+  // Обработка aria атрибута
+  let pressed = e.target.getAttribute('aria-pressed') === 'true';
+  e.target.setAttribute('aria-pressed', String(!pressed));
 };
