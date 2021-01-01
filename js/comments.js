55fa053669e792819f65f77fd53260076645ae64
Valentina-Aktanko <valentina.aktanko.khv@gmail.com>
2021-01-01 23:12:55 +0300

    feat: validates the comment-form - checks the number of characters in the comment

--- js/comments.js
+++ js/comments.js
@@ -2,9 +2,20 @@ let commentForm = document.querySelector('.comment-form');
 let commentList = document.querySelector('.comment-list');
 let commentField = document.querySelector('.comment-field');
 let charCounter = document.querySelector('.char-counter');
+let submitButton = document.querySelector('.submit-button');
 
 function setCharCount() {
   charCounter.textContent = commentField.value.length;
+}
+
+function checkLengthOfComment(maxValue = 142) {
+  if (commentField.value.length > 142) {
+    commentForm.classList.add('warning');
+    submitButton.disabled = true;
+  } else {
+    commentForm.classList.remove('warning');
+    submitButton.disabled = false;
+  }
 }
 
 commentForm.onsubmit = function (evt) {
@@ -15,14 +26,16 @@ commentForm.onsubmit = function (evt) {
   newComment.textContent = commentField.value;
   commentField.value = '';
   commentList.append(newComment);
+  charCounter.textContent = 0;
 };
 
 window.onload = function() {
   if (commentField.value.length) {
-    setCharCount()
+    setCharCount();
   }
 }
 
 commentField.oninput = function () {
-  setCharCount()
+  setCharCount();
+  checkLengthOfComment();
 };
